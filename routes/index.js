003c447b33dfaa6cc5b3643cccbb02d10a9cc546
braveek1 <braveek1@gmail.com>
2019-08-24 00:24:00 +0900

    edit comment

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var router = express.Router()
 
 /* GET home page. */
-// test4
+// test4 hoho my name is yongky
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" })
 })
