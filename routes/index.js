44d9c2d0a28c603b4e525e3cb3a7c6edc2b229ef
braveek1 <braveek1@gmail.com>
2019-08-24 00:35:54 +0900

    Merge branch 'yongkiTestAtHome2' into yongkiTestAtHome
    # Conflicts:
    #	routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var router = express.Router()
 
 /* GET home page. */
-// test4
+// test4 haho my name is yongki
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" })
 })
