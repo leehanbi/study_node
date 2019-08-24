9a46df3260cf1101d7b9486037f546094b049cb2
leehanbi <dlgksql05@gmail.com>
2019-08-24 12:14:06 +0900

    2

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ var router = express.Router()
 // test hanbi my name is hanbi aaa commit
 // a_a_branch
 // test c_branch aaaa
-// 1
+// 3
+// 2
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" })
 })
