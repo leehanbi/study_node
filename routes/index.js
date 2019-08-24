08776247dba38500c3923a7d9b790383fee8a486
leehanbi <dlgksql05@gmail.com>
2019-08-24 10:48:55 +0900

    // test 2 my name is hanbi 22222 comment commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router()
 /* GET home page. */
 // introduce myself
 // test haha my name is yongki
-// test 2 my name is hanbi
+// test 2 my name is hanbi 22222
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" })
 })
