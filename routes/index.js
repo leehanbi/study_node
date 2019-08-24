2b19e7ce0fd6bb24e7e7a6aee219f77beebb2ede
leehanbi <dlgksql05@gmail.com>
2019-08-24 10:41:01 +0900

    // test c_branch comment commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router()
 /* GET home page. */
 // introduce myself
 // test haha my name is yongki
+// test c_branch
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" })
 })
